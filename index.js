492bd07d103149d1d6e927472edfad4f081a676f
Heather Roberts <heather.roberts@3plearning.com>
2018-04-09 16:39:22 +1000

    Align messages nicely
    Line up each section (line, type, error message, rule id) for all messages for a file's linting errors

--- index.js
+++ index.js
@@ -5,22 +5,52 @@ const chalk = require('chalk');
 
 const cli = new CLIEngine();
 
+/**
+ * If string's length is less than longest, appends spaces until it reaches the
+ * same length
+ */
+const padString = (string, longest) => {
+  if (string.length < longest) {
+    Array(longest - string.length)
+    .fill(null)
+    .forEach(() => { string += ' '; });
+  }
+  return string;
+};
+
+/**
+ * Logs the linting results to the console
+ */
 const logResults = (filePath, messages, type, colour) => {
   console.log(chalk.bold[colour](`\n${type.toUpperCase()} in ${filePath}`));
 
   const relativePath = path.relative(process.cwd(), filePath);
   console.log(chalk.bold[colour](`\n${relativePath}`));
 
+  // find the messages with the longest strings for fields so we can nicely align them
+  const findLargestReducer = (prev, curr) => {
+    return curr > prev ? curr : prev;
+  };
+  const longestLineChars = messages
+  .map(message => `${message.line}:${message.column}`.length)
+  .reduce(findLargestReducer, 0);
+  const longestMsgChars = messages
+  .map(message => message.message.length)
+  .reduce(findLargestReducer, 0);
+
   messages.forEach((message) => {
-    const line = chalk.gray(`${message.line}:${message.column}`);
+    const lineText = padString(`${message.line}:${message.column}`, longestLineChars);
+    const line = chalk.gray(lineText);
     type = chalk[colour](type);
-    const error = chalk.white(message.message);
+    const errorTxt = padString(message.message, longestMsgChars);
+    const error = chalk.white(errorTxt);
     const rule = chalk.gray(message.ruleId);
-    console.log(`${line} ${type} ${error} ${rule}`);
+
+    console.log(`  ${line}  ${type}  ${error}  ${rule}`);
   });
 };
 
-const lint = (preprocessor) => (file) => {
+const lint = preprocessor => (file) => {
   const report = cli.executeOnFiles([file.filePath]);
   if (report.errorCount > 0) {
     const errorMsgs = report.results[0].messages.filter(result => result.severity === 2);
