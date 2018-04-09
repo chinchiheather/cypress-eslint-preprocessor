85d9d1ca1e29a8f5f7288055e4a163b2ed547fb2
Heather Roberts <heather.roberts@3plearning.com>
2018-04-09 15:24:26 +1000

    Add colours to error messages

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
-const browserifyPreprocessor = require('@cypress/browserify-preprocessor');
-const fs = require('fs');
-const CLIEngine = require('eslint').CLIEngine;
-const cli = new CLIEngine();
 const path = require('path');
+const browserifyPreprocessor = require('@cypress/browserify-preprocessor');
+const CLIEngine = require('eslint').CLIEngine;
+const chalk = require('chalk');
+
+const cli = new CLIEngine();
 
 const lint = (preprocessor) => (file) => {
   const report = cli.executeOnFiles([file.filePath]);
   if (report.errorCount > 0) {
-    console.log(`\nERROR in ${file.filePath}`);
+    console.log(chalk.bold.red(`\nERROR in ${file.filePath}`));
 
     const errorResults = report.results.filter(result => result.errorCount > 1);
     errorResults.forEach((result) => {
-      console.log(`\n${path.relative(process.cwd(), result.filePath)}`);
+      const relativePath = path.relative(process.cwd(), result.filePath);
+      console.log(chalk.bold.red(`\n${relativePath}`));
+
       result.messages.forEach((message) => {
         // todo: format with nice colours
-        console.log(`${message.line}:${message.column} error ${message.message} ${message.ruleId}`);
+        const line = chalk.gray(`${message.line}:${message.column}`);
+        const type = chalk.red('error');
+        const error = chalk.white(message.message);
+        const rule = chalk.gray(message.ruleId);
+        console.log(`${line} ${type} ${error} ${rule}`);
       });
     });
   }
+
+  // todo: warnings
 
   if (!preprocessor) {
     preprocessor = browserifyPreprocessor(browserifyPreprocessor.defaultOptions);
