57b4fab7fbf5e1f6059f6be21500d2f95100ae8b
Heather Roberts <heather.roberts@3plearning.com>
2018-04-09 15:51:13 +1000

    Handle warnings differently to errors
    Use different colour and text 'warning'

--- index.js
+++ index.js
@@ -5,28 +5,32 @@ const chalk = require('chalk');
 
 const cli = new CLIEngine();
 
+const logResults = (filePath, messages, type, colour) => {
+  console.log(chalk.bold[colour](`\n${type.toUpperCase()} in ${filePath}`));
+
+  const relativePath = path.relative(process.cwd(), filePath);
+  console.log(chalk.bold[colour](`\n${relativePath}`));
+
+  messages.forEach((message) => {
+    const line = chalk.gray(`${message.line}:${message.column}`);
+    type = chalk[colour](type);
+    const error = chalk.white(message.message);
+    const rule = chalk.gray(message.ruleId);
+    console.log(`${line} ${type} ${error} ${rule}`);
+  });
+};
+
 const lint = (preprocessor) => (file) => {
   const report = cli.executeOnFiles([file.filePath]);
   if (report.errorCount > 0) {
-    console.log(chalk.bold.red(`\nERROR in ${file.filePath}`));
-
-    const errorResults = report.results.filter(result => result.errorCount > 1);
-    errorResults.forEach((result) => {
-      const relativePath = path.relative(process.cwd(), result.filePath);
-      console.log(chalk.bold.red(`\n${relativePath}`));
-
-      result.messages.forEach((message) => {
-        // todo: format with nice colours
-        const line = chalk.gray(`${message.line}:${message.column}`);
-        const type = chalk.red('error');
-        const error = chalk.white(message.message);
-        const rule = chalk.gray(message.ruleId);
-        console.log(`${line} ${type} ${error} ${rule}`);
-      });
-    });
+    const errorMsgs = report.results[0].messages.filter(result => result.severity === 2);
+    logResults(file.filePath, errorMsgs, 'error', 'red');
   }
 
-  // todo: warnings
+  if (report.warningCount > 0) {
+    const warningMsgs = report.results[0].messages.filter(result => result.severity === 1);
+    logResults(file.filePath, warningMsgs, 'warning', 'yellow');
+  }
 
   if (!preprocessor) {
     preprocessor = browserifyPreprocessor(browserifyPreprocessor.defaultOptions);
